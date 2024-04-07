3c5088342dd4e9257cdb5f485c74fe5c44dceceb
princemakaza <zpmakaza@gmail.com>
2024-04-07 13:22:07 +0200

    docker added with

--- src/main/java/com/clinpride/SecurityPostgres/SecurityPostgresApplication.java
+++ src/main/java/com/clinpride/SecurityPostgres/SecurityPostgresApplication.java
@@ -2,12 +2,21 @@ package com.clinpride.SecurityPostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 @SpringBootApplication
 public class SecurityPostgresApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityPostgresApplication.class, args);
+
 	}
 
 }
